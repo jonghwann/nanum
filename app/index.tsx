2280f5117d2ad15704f6950f71468ae14497abf4
jonghwann <jonghwan0729@gmail.com>
2026-01-03 17:55:20 +0900

    chore: 프로젝트 초기 세팅

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to Nanum</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
